847a85a43bcccad74f6ac89c5c1accc997696332
dmitryturovtsov <web.best.master@gmail.com>
2019-11-19 15:27:38 +0300

    localize - refactor

--- www/js/component/locale/translation/en-us/data.js
+++ www/js/component/locale/translation/en-us/data.js
@@ -6,22 +6,21 @@ export const enUs: LangDataType = {
     /* eslint-disable id-match, id-length */
     META__LANGUAGE_NAME: 'English',
 
-    RESET_PASSWORD__PLEASE_ENTER_YOUR_NEW_PASSWORD: '[EN] Please enter your new password:',
-    RESET_PASSWORD__PASSWORD: '[EN] Password',
-    RESET_PASSWORD__CONFIRM_PASSWORD: '[EN] Confirm password',
-    RESET_PASSWORD__SET_PASSWORD: '[EN] Set Password',
-    RESET_PASSWORD__AT_LEAST_1_LETTER: '[EN] At least 1 letter',
-    RESET_PASSWORD__AT_LEAST_1_NUMBER: '[EN] At least 1 number',
-    RESET_PASSWORD__8_SYMBOLS_MINIMUM: '[EN] 8 symbols minimum',
+    RESET_PASSWORD__PLEASE_ENTER_YOUR_NEW_PASSWORD: 'Please enter your new password:',
+    RESET_PASSWORD__PASSWORD: 'Password',
+    RESET_PASSWORD__CONFIRM_PASSWORD: 'Confirm password',
+    RESET_PASSWORD__SET_PASSWORD: 'Set Password',
+    RESET_PASSWORD__AT_LEAST_1_LETTER: 'At least 1 letter',
+    RESET_PASSWORD__AT_LEAST_1_NUMBER: 'At least 1 number',
+    RESET_PASSWORD__8_SYMBOLS_MINIMUM: '8 symbols minimum',
     RESET_PASSWORD__PASSWORD_AND_CONFIRM_PASSWORD_SHOULD_BE_THE_SAME:
-        '[EN] Password and confirm password should be the same',
-    RESET_PASSWORD__CONFIRM_PASSWORD_IS_REQUIRED: '[EN] Confirm password is required',
+        'Password and confirm password should be the same',
+    RESET_PASSWORD__CONFIRM_PASSWORD_IS_REQUIRED: 'Confirm password is required',
 
-    RESET_PASSWORD_SUCCESS__SUCCESS: '[EN] Success!',
-    RESET_PASSWORD_SUCCESS__PASSWORD_WAS_SUCCESSFULLY_CHANGED: '[EN] Password was successfully changed.',
-    RESET_PASSWORD_SUCCESS__PLEASE_SING_IN_WITH_WITH_THE_NEW_PASSWORD:
-        '[EN] Please, sing in with with the new password.',
-    RESET_PASSWORD_SUCCESS__SIGN_IN: '[EN] Sign In',
+    RESET_PASSWORD_SUCCESS__SUCCESS: 'Success!',
+    RESET_PASSWORD_SUCCESS__PASSWORD_WAS_SUCCESSFULLY_CHANGED: 'Password was successfully changed.',
+    RESET_PASSWORD_SUCCESS__PLEASE_SING_IN_WITH_WITH_THE_NEW_PASSWORD: 'Please, sing in with with the new password.',
+    RESET_PASSWORD_SUCCESS__SIGN_IN: 'Sign In',
 
     /* eslint-enable id-match, id-length */
 };
